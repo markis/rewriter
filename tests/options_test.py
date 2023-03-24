0ab12877b96b46cca735836448e07e9c3e7f5855
Markis Taylor <m@rkis.net>
2023-03-23 22:46:23 -0400

    fix: expand test coverage

---
+++ tests/options_test.py
@@ -0,0 +1,17 @@
+import pytest
+
+from rewriter.options import parse_options
+
+
+def test_options_parsing() -> None:
+    args = parse_options(["-d", "-v", "test.py"])
+
+    assert args.dry_run
+    assert args.verbose
+    assert args.filename
+
+
+def test_help() -> None:
+    with pytest.raises(SystemExit):
+        parse_options(["--help"])
+        assert False, "Shouldn't make it here"
