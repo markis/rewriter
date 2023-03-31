c08f18e6317d932dff1849a560a75bc1e0991843
Markis Taylor <m@rkis.net>
2023-03-30 22:29:23 -0400

    fix: test

--- rewriter/__init__.py
+++ rewriter/__init__.py
@@ -1,8 +1,8 @@
 import os
 import sys
-from argparse import REMAINDER, SUPPRESS, ArgumentParser
+from argparse import ArgumentParser
 from pathlib import Path
-from typing import Any
+from typing import Any, cast
 
 from rewriter.options import Options
 from rewriter.parser import parse_tree, unparse_tree
@@ -29,29 +29,20 @@ load_transformers(os.path.dirname(__file__))
 def parse_options(args: Any = sys.argv[1:]) -> Options:
     is_compiled = "yes" if COMPILED else "no"
     parser = ArgumentParser(
-        prog="rewriter", description=f"Rewrite python code (compiled: {is_compiled})"
+        prog="rewriter",
+        description=f"Rewrite python code and fix issues (compiled: {is_compiled})",
     )
-    parser.add_argument("filenames", help="files to rewrite", nargs=REMAINDER)
-    parser.add_argument("filename", default="", help=SUPPRESS)  # internally used in iterator
-    parser.add_argument(
-        "-d",
-        "--dry-run",
-        action="store_true",
-        help="execute rewrite but don't update the original file",
-    )
+    parser.add_argument("filenames", help="files to rewrite", nargs="+")
+    parser.add_argument("-d", "--dry-run", action="store_true", help="skip writing the file")
     parser.add_argument("-v", "--verbose", action="store_true", help="print diff changes")
 
-    parsed_args: Any = parser.parse_args(args)
-    opts: Options = parsed_args
-    return opts
+    return cast(Options, parser.parse_args(args))
 
 
 def main() -> None:
     opts = parse_options()
-
     for file in opts.filenames:
         opts.filename = file
-
         tree = parse_tree(opts)
         changes, imports = walk(transform, tree)
         unparse_tree(opts, tree, changes, imports)
