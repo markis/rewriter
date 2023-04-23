1fe7886b3ddc3d785f211e674c53fb9bc28f6c2a
Markis Taylor <m@rkis.net>
2023-04-23 16:15:57 -0400

    refactor(tests): use snapshot tests for code generation testing (#30)

--- tests/rewrite_test.py
+++ tests/rewrite_test.py
@@ -2,76 +2,39 @@ import ast
 from textwrap import dedent
 from unittest.mock import Mock
 
+from syrupy.assertion import SnapshotAssertion
+
 from rewriter.options import Options
 from rewriter.parser import unparse_tree
 from rewriter.transformers import transform
 from rewriter.walker import walk
 
 
-def format_str(source: str) -> str:
-    return dedent(source).lstrip()
-
-
-def generate_new_source(source: str) -> str:
+def rewrite_code(source: str) -> str:
+    source = dedent(source).lstrip()
     mock_opts = Mock(spec=Options, filename="test.py", source=source, dry_run=True, verbose=True)
     tree = ast.parse(source)
-
     changes, imports = walk(transform, tree)
-
     return unparse_tree(mock_opts, tree, changes, imports)
 
 
-def test_func_definition() -> None:
-    source = format_str(
-        """
-        from __future__ import annotations
+def test_func_definition(snapshot: SnapshotAssertion) -> None:
+    source = """
+    def test(x, *args, **kwargs):
+        pass
+    """
+
+    assert rewrite_code(source) == snapshot
 
 
-        def test(x, *args, **kwargs):
+def test_class_definition(snapshot: SnapshotAssertion) -> None:
+    source = """
+    class Test:
+        def __init__(self, *, x):
             pass
-        """
-    )
-    expected = format_str(
-        """
-        from __future__ import annotations
-        from typing import Any
 
+        def do(self, x):
+            return 1 / 2
+    """
 
-        def test(x: Any, *args: Any, **kwargs: Any) -> None:
-            pass
-        """
-    )
-
-    assert generate_new_source(source) == expected
-
-
-def test_class_definition() -> None:
-    source = format_str(
-        """
-        from __future__ import annotations
-
-
-        class Test:
-            def __init__(self, *, x):
-                pass
-
-            def do(self, x):
-                return 1 / 2
-        """
-    )
-    expected = format_str(
-        """
-        from __future__ import annotations
-        from typing import Any
-
-
-        class Test:
-            def __init__(self, *, x: Any) -> None:
-                pass
-
-            def do(self, x: Any) -> Any:
-                return 1 / 2
-        """
-    )
-
-    assert generate_new_source(source) == expected
+    assert rewrite_code(source) == snapshot
