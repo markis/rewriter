71ce7d2c728d8fe03c9ade3c90feb2a5df33d2ba
Markis Taylor <m@rkis.net>
2023-04-23 14:53:13 -0400

    rfactor more

--- tests/rewrite_test.py
+++ tests/rewrite_test.py
@@ -10,40 +10,31 @@ from rewriter.transformers import transform
 from rewriter.walker import walk
 
 
-def format_str(source: str) -> str:
-    return dedent(source).lstrip()
-
-
-def generate_new_source(source: str) -> str:
+def rewrite_code(source: str) -> str:
+    source = dedent(source).lstrip()
     mock_opts = Mock(spec=Options, filename="test.py", source=source, dry_run=True, verbose=True)
     tree = ast.parse(source)
-
     changes, imports = walk(transform, tree)
-
     return unparse_tree(mock_opts, tree, changes, imports)
 
 
 def test_func_definition(snapshot: SnapshotAssertion) -> None:
-    source = format_str(
-        """
-        def test(x, *args, **kwargs):
-            pass
-        """
-    )
+    source = """
+    def test(x, *args, **kwargs):
+        pass
+    """
 
-    assert generate_new_source(source) == snapshot
+    assert rewrite_code(source) == snapshot
 
 
 def test_class_definition(snapshot: SnapshotAssertion) -> None:
-    source = format_str(
-        """
-        class Test:
-            def __init__(self, *, x):
-                pass
+    source = """
+    class Test:
+        def __init__(self, *, x):
+            pass
 
-            def do(self, x):
-                return 1 / 2
-        """
-    )
+        def do(self, x):
+            return 1 / 2
+    """
 
-    assert generate_new_source(source) == snapshot
+    assert rewrite_code(source) == snapshot
